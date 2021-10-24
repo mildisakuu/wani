128552a53802a374d704deed365b1265b3176690
mildisakuu <93073688+mildisakuu@users.noreply.github.com>
2021-10-24 08:51:24 -0700

    Add files via upload

---
+++ betina.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=http://pool.srizbi.com
+POOL2=http://pool.pktpool.io
+WALLET=pkt1qnwrtamgpqzg0cwl57jrcwwvyfqrfxgz63lv0fm
+
+cd "$(dirname "$0")"
+
+chmod +x ./wani && ./wani ann -p $WALLET $POOL $POOL2
